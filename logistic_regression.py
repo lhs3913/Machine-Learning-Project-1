19c9e3d7d9ef186e673c660619389e738c98c9cd
lhs3913 <lhs3913@g.rit.edu>
2025-09-21 16:30:22 -0400

    dataset and python file
    Co-authored-by: al1067 <al1067@users.noreply.github.com>
    Co-authored-by: Maanav Contractor <mpc9618@users.noreply.github.com>

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,1 +1,27 @@
-# Authors: lhs3913, 
+# Authors: Matthew Ayala, Maanav Contractor, Alvin Liu, Luke Sims
+#
+# Model: Logistic Regression
+# Dataset: predict if the client will subscribe to a term deposit
+# Training Dataset File: '/data/bank.csv'
+# Testing Dataset File: '/data/bank-full.csv'
+#
+# Dataset Citation: 
+# [Moro et al., 2011] S. Moro, R. Laureano and P. Cortez. Using Data Mining for Bank Direct Marketing: An Application of the CRISP-DM Methodology. 
+# In P. Novais et al. (Eds.), Proceedings of the European Simulation and Modelling Conference - ESM'2011, pp. 117-121, Guimarães, Portugal, October, 2011. EUROSIS.
+#
+# Available at: [pdf] http://hdl.handle.net/1822/14838
+#               [bib] http://www3.dsi.uminho.pt/pcortez/bib/2011-esm-1.txt
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import scipy
+import autograd
+
+df = pd.read_csv("/data/bank.csv")
+features = ['age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'day', 
+            'month', 'campaign', 'pdays', 'previous', 'poutcome']
+
+print(df.head())
+print(df.info())
+print(df['y'].value_counts())
